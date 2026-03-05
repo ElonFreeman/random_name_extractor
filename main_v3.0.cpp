edf61ad9613cd7afbae7a42475d5107f62816bb5
ElonFreeman <zhanghao112358@outlook.com>
2026-03-05 15:44:52 +0800

    for my best friends

--- main_v3.0.cpp
+++ main_v3.0.cpp
@@ -1,81 +1,113 @@
 #include<iostream>
 #include <stdlib.h>
 #include <time.h>
+#include<windows.h>
 using namespace std;
 
+int how_many_stu=0;
 struct names
 {
     int serial;
     char name[30];
 };
 
+
 int dataset_preprocess(names *name_and_serial)
 {
     /*generate  the serial*/
-    for(int i=0;i<62;i++)
+    for(int i=0;i<how_many_stu;i++)
     {
-        name_and_serial[i].serial=i;
+        (name_and_serial+i)->serial=i;
     }
 
     /*read the namesheet and input the names*/
     FILE *nameset=fopen("names.txt","r");
     if(nameset==nullptr)
     {
-        cout << "file open error!";
+        cout << "File open error!";
         return 1;
     }
-    for(int i=0;i<62;i++)
+    for(int i=0;i<how_many_stu;i++)
     {
-        fscanf(nameset,"%s",name_and_serial[i].name);
+        if(fscanf(nameset,"%s",(name_and_serial+i)->name)==EOF)
+        {
+            cout << "The number of student is error,please check and reinput:";
+            return 1;
+        }
     }
 
     return 0;
 }
 
+int check_dupli_item(int *logs,int rand_num,int i)
+{
+    int flag=0;
+    for(int j=0;j<i;j++)
+    {
+        if(rand_num==*(logs+j))
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    return flag;
+}
+
 int main(void)
 {
-    names name_and_serial[62]={};
-    dataset_preprocess(name_and_serial);
+    /*solve the garbled text,tell the sys that use utf-8 to process all of the things*/
+    SetConsoleOutputCP(CP_UTF8);
 
-    /*main*/
+    cout << "How many students in your class:";
+    cin >> how_many_stu;
+    names name_and_serial[how_many_stu]={};
+    int status=0;
+    status=dataset_preprocess(name_and_serial);
+    while(status==1)
+    {
+        cin >> how_many_stu;
+        name_and_serial[how_many_stu]={};
+        status=dataset_preprocess(name_and_serial);
+    }
+    
+
+    /*user options*/
     int num_of_name=0;
-    cout << "How many people do you want to extract?" << endl;
-    cin >> num_of_name;
-    while(!(num_of_name>0&&num_of_name<63))
+    cout << "How many people do you want to extract:";
+    cin >> num_of_name,cout << endl;
+    while(!(num_of_name>0&&num_of_name<(how_many_stu+1)))
     {
         cout << "Illeagally input!" << endl << "Please reinput:";
         cin >> num_of_name;
     }
 
-    int logs[num_of_name]={};
+    int logs[num_of_name]={};  //antiduplicate logs
     srand(time(NULL));  //intalize the seed for random num
     for(int i=0;i<num_of_name;i++)
     {
-        int flag=0;
-        /*generate random number*/
-        int rand_num=rand() % 61 + 0;
-        for(int j=0;j<i;j++)
-        {
-            if(rand_num==logs[j])
-            {
-                i--;
-                flag=1;
-                break;
-            }
-        }
+        /*generate the random number*/
+        int rand_num=rand()%(how_many_stu-1)+0;
+
+        /*check the duplicate items*/
+        int flag=check_dupli_item(logs,rand_num,i);
         if(flag==1)
         {
+            i--;
             continue;
         }
-
         logs[i]=rand_num;
+
         /*output the name which is extracted*/
         cout << name_and_serial[rand_num].name << endl;
     }
 
-    while(getchar() != '\n'){};  //放闪退机制
-    printf("\nPress any key to kill the terminal...");
-    getchar();
+    {
+        /*anticrash machanism*/
+        while(getchar() != '\n'){};
+        printf("\nPress any key to kill the terminal...");
+        getchar();
+    }
 
     return 0;
 }
